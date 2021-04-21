cbe6093a98e1e5296831ffd034150461b8d64061
a-bom <56231568+a-bom@users.noreply.github.com>
2021-04-21 17:40:24 -0600

    Update a-bom-js.js

--- a-bom-js.js
+++ a-bom-js.js
@@ -1,3 +1,9 @@
 function myFunction(x) {
   x.classList.toggle("change");
+  var x = document.getElementById("menu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
 }
