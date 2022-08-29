45ce0129ab9afe5e40822f5ea68fa174c10260b0
Annotated Book of Mormon <annotatedbom@gmail.com>
2022-08-29 14:53:06 -0600

    put cr before copyCultText()

--- a-bom.js
+++ a-bom.js
@@ -11,6 +11,7 @@ function hamburgerChange(x) {
 }
 
 document.getElementById("henryb").addEventListener("click", copyCultText)
+
 function copyCultText() {
   /* Get the text field */
   var imgText = "YOU CAN PRAY TO KNOW HIS WILL,\nand with the honest desire to do whatever He asks you to do, you will receive an answer.\nPresident Henry B. Eyring\nYou Are Not Alone in the Work\nOctober 2015 General Priesthood Session\r\rA COMMON TECHNIQUE AMONG RELIGIOUS CULTS\nis to instruct people to ask God what He wants them to do. Members are exhorted to study and pray in order to know God’s will for them. It is always implied that joining the group is God’s will and leaving the group is betraying it. Of course, if a person tells the cult leader that God is warning him to leave, this will not be accepted as valid.\nSteven Hassan\nCombatting Cult Mind Control, p. 70, 1988\nwww.freedomofmind.com";
