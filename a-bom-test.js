6587825bce5696e41b34d11ed27078e4cf2c7a6b
Annotated Book of Mormon <annotatedbom@gmail.com>
2022-09-08 13:32:25 -0600

    test new js code

---
+++ a-bom-test.js
@@ -0,0 +1,61 @@
+// If statement so that if id is not found, returns undefined instead of error
+let hamburger = document.getElementById('hamburger');
+if(hamburger){
+  hamburger.addEventListener("click", hamburgerChange);
+}
+
+// Allows the toggle between the hamburger icon and an "x" box.
+function hamburgerChange() {
+  let y = document.getElementById("hamburger");
+  y.classList.toggle("change");
+  let x = document.getElementById("menu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
+
+
+  const imgTextEng = "YOU CAN PRAY TO KNOW HIS WILL,\nand with the honest desire to do whatever He asks you to do, you will receive an answer.\nPresident Henry B. Eyring\nYou Are Not Alone in the Work\nOctober 2015 General Priesthood Session\r\rA COMMON TECHNIQUE AMONG RELIGIOUS CULTS\nis to instruct people to ask God what He wants them to do. Members are exhorted to study and pray in order to know God’s will for them. It is always implied that joining the group is God’s will and leaving the group is betraying it. Of course, if a person tells the cult leader that God is warning him to leave, this will not be accepted as valid.\nSteven Hassan\nCombatting Cult Mind Control, p. 70, 1988\nwww.freedomofmind.com";
+
+  const imgTextPor = "VOCÊS PODEM ORAR PARA CONHECER SUA VONTADE\ne, com o sincero desejo de fazer qualquer coisa que Ele lhes peça, vocês receberão uma resposta.\nPresidente Henry B. Eyring\nVocê Não Está Sozinho no Trabalho\nSessão Geral do Sacerdócio de outubro de 2015\r\rUM MÉTODO COMUM ENTRE OS CULTOS RELIGIOSOS\né instruir as pessoas a perguntarem a Deus o que Ele quer que elas façam. Os membros são exortados a estudar e orar para conhecer a vontade de Deus para eles. Está sempre implícito que ingressar no grupo é a vontade de Deus e deixar o grupo é traí-lo. Claro, se uma pessoa disser ao líder do culto que Deus o está avisando para sair, isso não será aceito como válido.\nSteven Hassan\nCombatting Cult Mind Control, p. 70, 1988\nwww.freedomofmind.com";  
+
+// If statement so that if id is not found, returns undefined instead of error
+let henryb = document.getElementById('henryb');
+if(henryb) {
+  henryb.addEventListener("click", function () { copyCultText(imgTextEng) } );
+}
+
+let henrybpor = document.getElementById('henrybpor');
+if(henrybpor) {
+  henrybpor.addEventListener("click", function () { copyCultText(imgTextPor) } );
+}
+
+// Copies the text found in the henryb image (as determined by the event listener) to clipboard.
+function copyCultText(text) {
+
+  // Copies the text:
+  navigator.clipboard.writeText(text);
+  
+  // Alert of the copied text:
+  alert("The following text was just copied:\r\r" + text);
+}
+
+
+
+// If statement so that if id is not found, returns undefined instead of error
+let hideBtn = document.getElementById('hidebtn');
+if(hideBtn){
+  hideBtn.addEventListener("click", hideIt);
+}
+
+// Allows the toggles of #hide between being displayed and not being displayed.
+function hideIt() {
+  let y = document.getElementById("hide");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
